6ff765d1fa651f61780934d61e02fbf6053bd316
Natalie Arnold <narnold@nevada.unr.edu>
2018-08-28 19:48:43 -0700

    PA0 and PA1

---
+++ computer-graphics/PA0/include/object.h
@@ -0,0 +1,28 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle;
+};
+
+#endif /* OBJECT_H */
