afda73298f1a7c881d0b58299df6c04dd755b357
Natalie Arnold <narnold@nevada.unr.edu>
2018-12-11 17:41:12 -0800

    map and sound

---
+++ computer-graphics/PA11/build/CMakeFiles/Wizard_Duel.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wizard_Duel.dir/src/camera.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/engine.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/graphics.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/imgui.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/imgui_demo.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/imgui_draw.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/imgui_impl_sdl.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/imgui_widgets.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/main.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/mesh.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/object.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/shader.cpp.o"
+  "CMakeFiles/Wizard_Duel.dir/src/window.cpp.o"
+  "Wizard_Duel.pdb"
+  "Wizard_Duel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wizard_Duel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
